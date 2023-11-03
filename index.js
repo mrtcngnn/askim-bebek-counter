c0eb786ed70a4bd4b0823ff7c4bd361b4a153d9b
Mert Can Gönen <71656016+mrtcngnn@users.noreply.github.com>
2023-11-03 15:04:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,26 +15,19 @@ window.onload = () => {
 
 var countDownDate = new Date("Jun 19, 2025 00:00:00").getTime();
 
-// Update the count down every 1 second
 var x = setInterval(function() {
 
-    // Get today's date and time
     var now = new Date().getTime();
-
-    // Find the distance between now and the count down date
     var distance = countDownDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Display the result in the element with id="demo"
     document.getElementById("day-value").innerHTML = days;
     document.getElementById("hour-value").innerHTML = hours;
     document.getElementById("minute-value").innerHTML = minutes;
     document.getElementById("second-value").innerHTML = seconds;
 
-    // If the count down is finished, write some text
 }, 1000);
